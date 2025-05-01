49ee6765c4f17e9dbc4ba387b9b6d1f5c5ec52fb
AkanshaNagariya <145829960+AkanshaNagariya@users.noreply.github.com>
2025-05-01 23:43:19 +0530

    Add files via upload

---
+++ clearbitmanupilation.java
@@ -0,0 +1,14 @@
+public class clearbitmanupilation {
+    public static void main(String[] args) {
+        // clear bit
+        // Q. clear the 3rd bit (pos = 2 from right to left in num 5) of number n , n= 0101 i.e 5
+        int n = 5;  // given number 5 i.e 0101
+        int pos = 2; // position upto which we will left shift 1 i.e 0001 
+        int bitMask = 1<<pos; // left shit 1 
+        int notBitMask = ~(bitMask); // not operation 
+
+        int newNumber = notBitMask &  n;  // and of n and bitmask gives bit on that pos , and of n and not of bitmask will change
+                                           // that bit , 1 to 0 and viceversa so bit will be clear
+        System.err.println(newNumber);
+    }
+}
