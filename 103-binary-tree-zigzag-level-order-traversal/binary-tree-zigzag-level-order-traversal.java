6411f623925eb3186c0202fe677b8d7c8fbb6336
Akansha Nagariya <145829960+AkanshaNagariya@users.noreply.github.com>
2025-06-06 22:25:54 +0530

    Time: 1 ms (67.32%) | Memory: 42.1 MB (89.49%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+         List<List<Integer>> res = new ArrayList<>();
+           if(root == null) {
+            return res;
+           }
+        Deque<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean reverse = false;
+        while(!q.isEmpty()){
+            int levelsize = q.size();
+            List<Integer> ans = new ArrayList<>(levelsize);
+            for(int i=0; i<levelsize;i++){
+                if(!reverse){
+                TreeNode currnode = q.pollFirst();
+                ans.add(currnode.val);
+                if(currnode.left != null){
+                    q.addLast(currnode.left);
+                }
+                if(currnode.right != null){
+                    q.addLast(currnode.right);
+                }
+            } else {
+                TreeNode currnode = q.pollLast();
+                ans.add(currnode.val);
+                if(currnode.right != null){
+                    q.addFirst(currnode.right);
+                }
+                if(currnode.left != null){
+                    q.addFirst(currnode.left);
+                }
+            }
+            }
+            reverse = !reverse;
+            res.add(ans);
+        }
+        return res; 
+    }
+}
