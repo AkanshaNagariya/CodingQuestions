4d20f83534843d8ce27907989f56907e8ee09c81
Akansha Nagariya <145829960+AkanshaNagariya@users.noreply.github.com>
2025-06-21 09:52:58 +0530

    Time: 2 ms (68.52%), Space: 42.3 MB (86.08%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,4 +1,4 @@
-class Pair{
+class Pair{  // created class pair to store row , col and time
     int row;
     int col;
     int tm;
@@ -10,42 +10,42 @@ class Solution {
     public int orangesRotting(int[][] grid) {
        int n = grid.length;
        int m = grid[0].length;
-       Queue<Pair> q = new LinkedList<>();
-       int [][] vis = new int[n][m];
-       int cntfresh = 0;
+       Queue<Pair> q = new LinkedList<>(); // queue will have row , col and time
+       int [][] vis = new int[n][m]; // visisted 2d array
+       int cntfresh = 0; // fresh oranges
        for(int i=0; i<n;i++){
         for(int j = 0; j<m; j++){
-            if(grid[i][j] == 2){
-                q.add(new Pair(i,j,0));
-                vis[i][j] = 2;
+            if(grid[i][j] == 2){ // rotten orange
+                q.add(new Pair(i,j,0)); // add in queue
+                vis[i][j] = 2; //mark visisted
             } else {
-                vis[i][j] = 0;
+                vis[i][j] = 0;  // empty cell
             }
             if(grid[i][j] == 1) cntfresh++;
         }
        } 
-       int tm = 0;
-       int drow[] = {-1,0,+1,0};
-       int dcol[] = {0,1,0,-1};
-       int cnt = 0;
+       int tm = 0;   // to cnt max time to rotten all 
+       int drow[] = {-1,0,+1,0}; // left right up down of row
+       int dcol[] = {0,1,0,-1}; // of col
+       int cnt = 0;  // the orange we rot now
        while(!q.isEmpty()){
-        int r = q.peek().row;
+        int r = q.peek().row;  // get row , col and t from queue
         int c = q.peek().col;
         int t = q.peek().tm;
-        tm = Math.max(t,tm);
-        q.remove();
-        for(int i=0; i<4;i++){
-            int nrow = r + drow[i];
+        tm = Math.max(t,tm); // keep updating max time to rot
+        q.remove(); // an element from queue and
+        for(int i=0; i<4;i++){ // get leftright updown row and col
+            int nrow = r + drow[i];  
             int ncol = c + dcol[i];
             if(nrow >= 0 && nrow < n && ncol >= 0 && ncol <m && vis[nrow][ncol]==0
-            && grid[nrow][ncol] == 1){
-                q.add(new Pair(nrow, ncol, t+1));
-                vis[nrow][ncol] = 2;
-                cnt++;
+            && grid[nrow][ncol] == 1){  // the orange is fresh i.e 1 and not visisted
+                q.add(new Pair(nrow, ncol, t+1)); // add in queue
+                vis[nrow][ncol] = 2;  // mark visisted
+                cnt++; // no of rotten
             }
         }
        }
-       if(cnt != cntfresh) return -1;
+       if(cnt != cntfresh) return -1;  // if we didn't rotten all oranges return -1
        return tm;
     }
 }
