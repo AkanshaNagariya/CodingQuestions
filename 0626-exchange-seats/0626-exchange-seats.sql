b7d22406302d3e7eceabd2293e2208c7938592dc
Akansha Nagariya <145829960+AkanshaNagariya@users.noreply.github.com>
2025-07-04 20:01:37 +0530

    Time: 352 ms (50.45%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,14 +1,15 @@
 # Write your MySQL query statement below
 select 
     case 
-        when
-           id = (select max(id) from seat) AND MOD(id,2)=1
-           Then id
         when 
-        MOD(id,2) = 1
-        THEN id+1
+           id = (select max(id) from seat) AND MOD(id,2)=1 # when we check last id i.e max and 
+           # it is odd so keep id as it is no change
+           Then id # same
+        when   # if it is not last id then check
+        MOD(id,2) = 1   # if id is odd no 
+        THEN id+1 # swap it with next id
         ELSE 
-            id - 1
-      END AS id , student
+            id - 1  # swap it with previous id
+      END AS id , student # show as id, student
 From Seat
 ORDER BY id 
