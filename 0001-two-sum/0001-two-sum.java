f2bf909092da0be4153fe04937efa873016f17ea
Akansha Nagariya <145829960+AkanshaNagariya@users.noreply.github.com>
2025-07-10 23:08:02 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer , Integer> map = new HashMap<>();
+        
         int [] ans = new int[2];
         ans[0] = -1;
         ans[1] = -1;
